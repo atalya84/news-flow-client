893a8b8bdd6fe184004de3493ac1319c90d53e01
Guy Moore <guymoore8@gmail.com>
2024-07-29 17:21:09 +0300

    use apiClient

--- src/services/posts.service.ts
+++ src/services/posts.service.ts
@@ -1,24 +1,20 @@
-import axios from 'axios';
 import { IPost, IPostInput } from '../types/feed.types';
-
-const client = axios.create({
-	baseURL: 'http://localhost:4000/posts',
-});
+import apiClient from './api-client';
 
 export const fetchPosts = async (): Promise<IPost[]> =>
-	(await client.get<IPost[]>('/')).data;
+	(await apiClient.get<IPost[]>('/posts')).data;
 
 export const fetchPost = async (postId: string): Promise<IPost> =>
-	(await client.get<IPost>(`/${postId}`)).data;
+	(await apiClient.get<IPost>(`/posts/${postId}`)).data;
 
 export const createPost = async (postInput: IPostInput): Promise<IPost> =>
-	(await client.post<IPost>('/', postInput)).data;
+	(await apiClient.post<IPost>('/posts', postInput)).data;
 
 export const updatePost = async (
 	postId: string,
 	postData: IPostInput,
-): Promise<IPost> => (await client.put<IPost>(`/${postId}`, postData)).data;
+): Promise<IPost> => (await apiClient.put<IPost>(`/posts/${postId}`, postData)).data;
 
 export const deletePost = async (postId: string): Promise<void> => {
-	(await client.delete(`/${postId}`)).data;
+	(await apiClient.delete(`/posts/${postId}`)).data;
 };
