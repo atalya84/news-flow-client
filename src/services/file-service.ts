2e4d246d62821ba02d12c6cd160ad2251f957243
Guy Moore <guymoore8@gmail.com>
2024-07-29 17:22:33 +0300

    use apiClient

--- src/services/file-service.ts
+++ src/services/file-service.ts
@@ -1,6 +1,5 @@
 import apiClient from './api-client';
 import { IUploadResponse } from '../types/File';
-import axios from 'axios';
 
 export async function uploadPhoto(formData: FormData): Promise<string> {
 	try {
@@ -16,9 +15,5 @@ export async function uploadPhoto(formData: FormData): Promise<string> {
 }
 
 export const uploadPostImage = async (formData: FormData): Promise<string> =>
-	(
-		await axios.post<IUploadResponse>(
-			'http://localhost:4000/file/uploadPost',
-			formData,
-		)
-	).data.imgUrl;
+	(await apiClient.post<IUploadResponse>('/file/uploadPost', formData)).data
+		.imgUrl;
