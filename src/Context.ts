c2fddc5104e4f515f1c747166a695771e237588e
Guy Moore <guymoore8@gmail.com>
2024-07-31 01:33:28 +0300

    moved all context to context.ts

--- src/Context.ts
+++ src/Context.ts
@@ -1,7 +1,21 @@
-import { createContext } from "react";
-import { IUser } from "./types/user.types";
+import { createContext, Dispatch, SetStateAction } from 'react';
+import { IUser } from './types/user.types';
+import { IPost } from './types/feed';
+import { GeoData, Weather } from './types/weather';
 
-export const AuthContext = createContext<{user: IUser | null, setUser: Function}>({
-    user: null,
-    setUser: () => {}
+export const AuthContext = createContext<{
+	user: IUser | null;
+	setUser: Function;
+}>({
+	user: null,
+	setUser: () => {},
 });
+
+export const WeatherContext = createContext<
+	[string, Dispatch<SetStateAction<string>>]
+>(['Rishon LeTsiyon', () => {}]);
+
+export const PostContext = createContext<{
+	post: IPost | undefined;
+	setPost: Dispatch<SetStateAction<IPost | undefined>>;
+} | null>(null);
