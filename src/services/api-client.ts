886f9c049c7513093f67fb5afb6cc8cced4b056c
atalya84 <atalyanagar@gmail.com>
2024-07-30 10:02:19 +0300

    api-client: fix header on formdata

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,9 +1,48 @@
-import axios, { CanceledError } from "axios";
+import axios from "axios";
 import { config } from "../config/config";
 
-export { CanceledError }
 const apiClient = axios.create({
     baseURL: config.DOMAIN_BASE
 });
+
+apiClient.interceptors.request.use(
+    config => {
+        const accessToken = localStorage.getItem('accessToken') || "";
+        const refreshToken = localStorage.getItem('refreshToken') || "";
+
+        config.headers['Authorization'] = `Bearer ${accessToken}`;
+        config.headers['refresh_token'] = refreshToken;
+
+        // Ensure Content-Type is only set if not sending FormData
+        if (!(config.data instanceof FormData) && !config.headers['Content-Type']) {
+            config.headers['Content-Type'] = 'application/json';
+        }
+
+        return config;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+);
+
+apiClient.interceptors.response.use(
+    res => res,
+    async error => {
+        if (error.response.status === 401 && error.config.url !== '/auth/refresh') {
+            const res = await apiClient.get('/auth/refresh');
+            console.log("REFRESH WORKED!!!");
+            localStorage.setItem('refreshToken', res.data.refreshToken);
+            localStorage.setItem('accessToken', res.data.accessToken);
+            return await apiClient(error.config); // retry failed request
+        } else if (error.response.status === 401 && error.config.url === '/auth/refresh') {
+            // if refresh returned 401, logout and return to home page
+            localStorage.removeItem('refreshToken');
+            localStorage.removeItem('accessToken');
+            window.location.href = '/';
+        }
+
+        return Promise.reject(error);
+    }
+);
 
 export default apiClient;
