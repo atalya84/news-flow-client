5e256c42806ebdff2a606f9729cf337ca343c36c
atalya <email@org.com>
2024-07-30 23:15:56 +0300

    fix favourite posts

---
+++ src/pages/myPosts/PopularPosts.tsx
@@ -0,0 +1,38 @@
+import { FC, useContext, useEffect, useState } from 'react';
+import FeedItem from '../../ui/FeedItem/FeedItem';
+import { IPost } from '../../types/feed';
+import { Grid, Typography } from '@mui/material';
+import { fetchPopPosts } from '../../services/posts.service';
+import { AuthContext } from '../../Context';
+
+export const PopularPosts: FC = () => {
+	const [posts, setPosts] = useState<IPost[]>([]);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const { user } = useContext(AuthContext);
+
+	useEffect(() => {
+		setIsLoading(true);
+		fetchPopPosts(user!._id)
+			.then(setPosts)
+			.catch(console.error)
+			.finally(() => setIsLoading(false));
+	}, []);
+
+	return (
+		<Grid container justifyContent={'center'}>
+			<Grid item xl={6} lg={12}>
+				{isLoading ? (
+					<Typography variant="h5" color={'white'}>
+						Loading...
+					</Typography>
+				) : posts.length === 0 ? (
+					<Typography variant="h5" color={'white'}>
+						{'No Posts Found :('}
+					</Typography>
+				) : (
+					posts.map((post, index) => <FeedItem post={post} key={index} />)
+				)}
+			</Grid>
+		</Grid>
+	);
+};
