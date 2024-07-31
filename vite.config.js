b5e232128ba0ef262014e1d17f2ffdce39d6a207
atalya84 <atalyanagar@gmail.com>
2024-07-31 19:17:52 +0300

    use https

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs'
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()]
+    plugins: [ react() ],
+    server: {
+        https: {
+            key: fs.readFileSync(`../client-key.pem`),
+            cert: fs.readFileSync(`../client-cert.pem`),
+        }
+    }
 });
